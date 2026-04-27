d5a84272f69fa6683c04400adb64e47d89c3d05e
ooi2572025 <ooi2572025@stu.o-hara.ac.jp>
2026-04-27 14:34:25 +0900

    beanがんばった

---
+++ src/main/java/bean/TestListStudent.java
@@ -0,0 +1,34 @@
+package bean;
+
+public class TestListStudent {
+
+    private String subjectName;
+    private String subjectCd;
+    private Integer no;
+    private Integer point;
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+    public String getSubjectCd() {
+        return subjectCd;
+    }
+    public void setSubjectCd(String subjectCd) {
+        this.subjectCd = subjectCd;
+    }
+    public Integer getNo() {
+        return no;
+    }
+    public void setNo(Integer no) {
+        this.no = no;
+    }
+    public Integer getPoint() {
+        return point;
+    }
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+}
---
+++ src/main/java/bean/TestListSubject.java
@@ -0,0 +1,39 @@
+package bean;
+
+public class TestListSubject {
+	public Integer getEntYear() {
+		return entYear;
+	}
+	public void setEntYear(Integer entYear) {
+		this.entYear = entYear;
+	}
+	public String getStudentNo() {
+		return studentNo;
+	}
+	public void setStudentNo(String studentNo) {
+		this.studentNo = studentNo;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getClassNum() {
+		return classNum;
+	}
+	public void setClassNum(String classNum) {
+		this.classNum = classNum;
+	}
+	public Integer getPoints() {
+		return points;
+	}
+	public void setPoints(Integer points) {
+		this.points = points;
+	}
+	private Integer entYear;
+	private String studentNo;
+	private String studentName;
+	private String classNum;
+	private Integer points;
+}
